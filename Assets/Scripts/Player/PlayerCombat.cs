c843e922725838f03bc9118cfe1e8a247707882f
dmlwjds2 <dmlwjd0809@hanyang.ac.kr>
2026-07-11 15:36:19 +0900

    feat: 플레이어 기본세팅

--- Assets/Scripts/Player/PlayerCombat.cs
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -2,42 +2,19 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-    [Header("히트스캔")]
+    [Header("투사체")]
     [SerializeField] private Transform firePoint;
     [SerializeField] private float attackCooldown = 0.3f;
-    [SerializeField] private float attackRange = 20f;
-    [SerializeField] private LayerMask hitLayers;
 
     [Header("스탯")]
     [SerializeField] private float attackDamage = 10f;
 
-    [Header("레이 시각화")]
-    [SerializeField] private LineRenderer lineRenderer;
-    [SerializeField] private float lineDuration = 0.05f;
-    [SerializeField] private Color hitColor = Color.red;
-    [SerializeField] private Color missColor = Color.yellow;
-
     private float lastAttackTime;
-    private float lineHideTime;
-
-    private void Awake()
-    {
-        if (lineRenderer == null)
-            lineRenderer = GetComponent<LineRenderer>();
-
-        lineRenderer.positionCount = 2;
-        lineRenderer.startWidth = 0.05f;
-        lineRenderer.endWidth = 0.05f;
-        lineRenderer.enabled = false;
-    }
 
     private void Update()
     {
         if (Input.GetMouseButton(0) && CanAttack())
             Shoot();
-
-        if (lineRenderer.enabled && Time.time >= lineHideTime)
-            lineRenderer.enabled = false;
     }
 
     private bool CanAttack()
@@ -49,30 +26,8 @@ public class PlayerCombat : MonoBehaviour
     {
         lastAttackTime = Time.time;
 
-        Vector2 origin = firePoint.position;
-        Vector2 direction = firePoint.up;
-        RaycastHit2D hit = Physics2D.Raycast(origin, direction, attackRange, hitLayers);
-
-        Vector2 endPoint;
-        if (hit.collider != null)
-        {
-            endPoint = hit.point;
-            lineRenderer.startColor = hitColor;
-            lineRenderer.endColor = hitColor;
-
-            if (hit.collider.TryGetComponent<IDamageable>(out var target))
-                target.TakeDamage(attackDamage);
-        }
-        else
-        {
-            endPoint = origin + direction * attackRange;
-            lineRenderer.startColor = missColor;
-            lineRenderer.endColor = missColor;
-        }
-
-        lineRenderer.SetPosition(0, origin);
-        lineRenderer.SetPosition(1, endPoint);
-        lineRenderer.enabled = true;
-        lineHideTime = Time.time + lineDuration;
+        GameObject obj = ProjectilePool.Instance.Get();
+        obj.transform.SetPositionAndRotation(firePoint.position, firePoint.rotation);
+        obj.GetComponent<Projectile>().Init(attackDamage);
     }
 }
